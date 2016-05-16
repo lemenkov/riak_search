e4b02e16834a6ada08bd01596d7d8895b5705abd
Peter Lemenkov <lemenkov@gmail.com>
2016-05-16 18:33:25 +0300

    Deprecated BIF - erlang:now/0
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/riak_search_ring_utils.erl
+++ src/riak_search_ring_utils.erl
@@ -52,7 +52,7 @@ get_range_preflist(NVal, VNodes, UpNodes) ->
     UpNodesSet = ordsets:from_list(UpNodes),
 
     %% Randomly rotate the vnodes...
-    _ = random:seed(now()),
+    _ = random:seed(os:timestamp()),
     RotationFactor = random:uniform(NVal),
     {Pre, Post} = lists:split(RotationFactor, VNodes),
     VNodes1 = Post ++ Pre,
